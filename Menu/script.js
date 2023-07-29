ffacabba0292ac77d3e57ac1519c3c6279e01b67
Ayça <46596708+aycaoktay@users.noreply.github.com>
2023-07-29 16:02:08 +0300

    Dobby Cafe Menu

--- Menu/script.js
+++ Menu/script.js
@@ -3,24 +3,24 @@ const menu =[
         id: 1 ,
         title: "Çok Özlü İksir",
         category : "İksir",
-        price : 85 ,
-        Image : "C:\Users\AYCA\OneDrive\Belgeler\GitHub\PatikaWorks\Menu\dessert.jpeg",
+        price : '85 TL' ,
+        Image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeVaH3KfauefkYaljN7yfEZM-DgWxkR3Nepg&usqp=CAU",
         desc : 'İçen kişiyi, iksirin içine katılan malzemeye göre başka bir kişiye döndürür '
  },
  {
     id: 2 ,
-    title: "Golden Snitch Kruvasan",
-    category : "Tatli",
-    price : 85 ,
-    Image : "C:\Users\AYCA\OneDrive\Belgeler\GitHub\PatikaWorks\Menu\WhatsApp Image 2023-07-28 at 15.34.40.jpeg",
-    desc : 'Çikolatalı enfes lezzet  '
+    title: "Kaymak Birası",
+    category : "İçecek",
+    price : '90 TL',
+    Image : "http://www.gazetebilkent.com/wp-content/uploads/2019/02/IMG-20190210-WA0092-768x1024.jpg",
+    desc : 'Büyücüler için leziz bir içecektir   '
 },
 {
     id: 3 ,
-    title: "Golden Snitch Kruvasan",
+    title:"Adamotu Tatlısı",
     category : "Tatli",
-    price : 85 ,
-    Image : "C:\Users\AYCA\OneDrive\Belgeler\GitHub\PatikaWorks\Menu\WhatsApp Image 2023-07-28 at 15.34.40.jpeg",
+    price : '120 TL' ,
+    Image : "https://www.eniyi.best/image/hogwarts-express-cafe-cankaya-ankara-4743-6674.jpg",
     desc : 'Çikolatalı enfes lezzet  '
 },
  
@@ -28,18 +28,18 @@ const menu =[
     id: 4 ,
     title: "Golden Snitch Kruvasan",
     category : "Tatli",
-    price : 85 ,
-    Image : "C:\Users\AYCA\OneDrive\Belgeler\GitHub\PatikaWorks\Menu\WhatsApp Image 2023-07-28 at 15.34.40.jpeg",
+    price : '125 TL' ,
+    Image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3D9Dg9vKdYsk_TEzNdxqMiW791usGfS14RZQGLRoMB4G0p-4SK6Xw2rYdKG0KxXnuoPg&usqp=CAU",
     desc : 'Çikolatalı enfes lezzet  '
 },
  
 {
     id: 5 ,
-    title: "Golden Snitch Kruvasan",
-    category : "Tatli",
-    price : 85 ,
-    Image : "C:\Users\AYCA\OneDrive\Belgeler\GitHub\PatikaWorks\Menu\WhatsApp Image 2023-07-28 at 15.34.40.jpeg",
-    desc : 'Çikolatalı enfes lezzet  '
+    title: "Gevezelik İksiri",
+    category : "İksir",
+    price : ' 68 TL' ,
+    Image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOjEM4H5vO6rGDqKztah32_4hTRVFBYrZ48w&usqp=CAU",
+    desc : 'Yaban Mersini ve Ejder meyvesi lezzetiyle...'
 },
 ];
 const section = document.querySelector(".section-center");
